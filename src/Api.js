8155f1fb9e1b541e19b80ae0a9b7eeb045094c81
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-12 15:43:40 +0000

    Get API component fetching peeps data from backend

--- src/Api.js
+++ src/Api.js
@@ -8,14 +8,16 @@ class App extends Component {
   // Code is inovked after the component is mounted/inserted into the DOM tree.
   componentDidMount() {
     const url =
-        'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+        'https://chitter-backend-api-v2.herokuapp.com/peeps'
 
     fetch(url)
       .then((result) => result.json())
       .then((result) => {
+        console.log(result)
         this.setState({
           data: result,
         })
+
       })
   }
 
@@ -23,7 +25,7 @@ class App extends Component {
     const {data} = this.state
 
     const result = data.map((entry, index) => {
-      return <li key={index}>{entry}</li>
+      return <li key={index}>{entry.body}</li>
     })
 
     return <ul>{result}</ul>
